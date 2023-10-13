8bca2da598f579410868d8e97e9366bb962da494
Keith Rozario <keithjosephrozario@gmail.com>
2023-10-13 13:28:07 +0800

    changed to stream

--- pages/3_Leave_feedback.py
+++ pages/3_Leave_feedback.py
@@ -1,14 +1,20 @@
 import streamlit as st
-from common_functions import submit_feedback
-
+import json
+from backend_functions.detect_sentiment import get_response
 
 st.write("This demo takes in feedback from user and performs sentinment analysis using AWS Comprehend")
 
 
 feedback_text = st.text_area("Leave feedback on your experience")
+
 if st.button("Submit Feedback"):
-    feedback_response = submit_feedback(
-        feedback_text = feedback_text,
-        id_token = "null"
-    )
-    st.write(f"You've given {feedback_response['Sentiment']} feedback")
+    # feedback_response = detect_sentiment(feedback_text)
+    # st.write(f"You've given {feedback_response['Sentiment']} feedback")
+
+    llm_stream_response = get_response(feedback_text)
+    placeholder = st.empty()
+    response = ""
+    for event in llm_stream_response:
+        chunk = json.loads(event['chunk']['bytes'].decode('utf-8'))['completion']
+        response += chunk
+        placeholder.write(response)
